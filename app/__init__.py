610708032cc6649433852906404ff536c68beafc
Charles Ludwig <charleswludwig@gmail.com>
2023-09-13 15:28:46 -0400

    updating login / registration ability for profile dashboard

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from .forms import LoginForm
 from flask_login import LoginManager
-from app.config import ConfigClass
+from .config import ConfigClass
 
 app = Flask(__name__)
 
@@ -13,13 +13,14 @@ app.config['JSON_SORT_KEYS'] = False
 app.config["DEBUG"] = True
 app.config["APPLICATION_ROOT"] = "/"
 
-login = LoginManager()
+login = LoginManager(app)
+login.login_view = 'login'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import routes
-from app.models import User, Events, Post
+from app import routes, forms, models 
+from .models import User, Events, Post
 
 @app.shell_context_processor
 def make_shell_context():
