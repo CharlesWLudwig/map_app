51f88dfae8456776930db29b37e75ee1fcac5abc
Charles Ludwig <124328094+CharlesWLudwig@users.noreply.github.com>
2023-10-19 11:25:30 -0400

    Update config.py

--- config.py
+++ config.py
@@ -9,23 +9,3 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# This is for online db (if you have a file for the connection string - .ini)
-"""
-def config_db(filename='database.ini', section='postgresql'):
-    # create a parser
-    parser = ConfigParser()
-    # read config file
-    parser.read(filename)
-
-    # get section, default to postgresql
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        pass
-    
-    return db
-"""
